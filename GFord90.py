31f8db27eaaa5e4ca6e2a531196f21b3fefb770a
ChristianStrebs <christianstreby@gmail.com>
2023-09-05 08:58:53 -0500

    Update GFord90.py
    This kinda Does some stuff like adding comments and things

--- GFord90.py
+++ GFord90.py
@@ -1,2 +1,6 @@
+#Alphabet String
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#The list will print the ASCII value of each character besides the letters in Grants name
 alphabetList = [i if i in ["G","R","A","N","T"] else print(f"the ASCII Value of {i} is {ord(i)}") for i in alphabet]
+print("feature 1")
+print("Jo MOmma")
