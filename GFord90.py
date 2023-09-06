3c22d65828d78fdde23520ca313292425d39f285
ChristianStrebs <143951047+ChristianStrebs@users.noreply.github.com>
2023-09-06 13:18:39 +0000

    Changed feature

--- GFord90.py
+++ GFord90.py
@@ -2,4 +2,4 @@
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #The list will print the ASCII value of each character besides the letters in Grants name
 alphabetList = [i if i in ["G","R","A","N","T"] else print(f"the ASCII Value of {i} is {ord(i)}") for i in alphabet]
-print("feature 1")
+print("Yur")
